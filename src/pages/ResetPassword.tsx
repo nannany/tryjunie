b2f7045dbf2d303c1834b8e3bfea6cdd6c9e1d9f
nannany <ymym1990ymym@gmail.com>
2025-04-08 17:48:23 +0900

    forgot password flow

---
+++ src/pages/ResetPassword.tsx
@@ -0,0 +1,11 @@
+import { ResetPasswordForm } from '@/components/reset-password-form'
+
+const ResetPassword = () => {
+  return (
+    <div className="max-w-md mx-auto py-10">
+      <ResetPasswordForm />
+    </div>
+  )
+}
+
+export default ResetPassword 
