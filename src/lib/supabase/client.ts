ca510b1cffa9ce37312bc3db2bf4eec399ffb68a
nannany <ymym1990ymym@gmail.com>
2025-04-08 14:58:46 +0900

    build 通るように

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -2,7 +2,9 @@ import { createClient as createSupabaseClient } from '@supabase/supabase-js'
 
 export function createClient() {
   return createSupabaseClient(
-    import.meta.env.VITE_SUPABASE_URL!,
+    // @ts-ignore: TS2339
+    import.meta.env.VITE_SUPABASE_URL!, 
+    // @ts-ignore: TS2339
     import.meta.env.VITE_SUPABASE_ANON_KEY!
   )
 }
